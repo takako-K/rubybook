85ca972dd05a3c9d8dd62bb322fa44d4593b75d9
takako-K <takako.k828@gmail.com>
2021-03-06 21:46:30 +0900

    [Add]chapter8 部品を作る クラス

---
+++ chapter8/private_public1.rb
@@ -0,0 +1,13 @@
+class Foo
+  def a
+  end
+  def b
+  end
+
+  private
+
+  def c
+  end
+  def d
+  end
+end
---
+++ chapter8/private_public2.rb
@@ -0,0 +1,18 @@
+class Foo
+  def a
+  end
+
+  def b
+  end
+
+  private
+
+  def c
+  end
+
+  public
+
+  def d
+  end
+end
+
