8cb2bdefebe745855655935751155b8d2b021ce8
takako-K <takako.k828@gmail.com>
2021-03-22 15:06:48 +0900

    [Finish]chapter2 かんたんなプログラムを書く

---
+++ chapter2α/chapter2 practice.rb
@@ -0,0 +1,43 @@
+# 2-1
+# 問1
+puts 2 + 3
+
+# 問2
+puts 2 * 2 * 3.14
+
+# 2-2
+# 問3
+puts "Ruby"
+
+# 問4
+puts "abc" + "def"
+
+# 問5
+puts "123".to_i + "456".to_i
+
+# 2-3
+# 問6
+coffee = 300
+espresso = 100
+order = coffee + espresso * 2
+puts "コーヒー：#{coffee}円"
+puts "合計：#{order}円"
+
+# 問7
+coffee = 400
+espresso = 100
+order = coffee + espresso * 2
+puts "コーヒー：#{coffee}円"
+puts "合計：#{order}円"
+
+# 2-6
+# 問8
+total = 300 * 2
+p total
+tax = total * 0.08
+puts tax
+
+# 2-7
+# 問9
+total = 300 * 2
+puts total
