df7e2d43c4e816d6afc9c80288ac639ffae23292
takako-K <takako.k828@gmail.com>
2021-04-01 20:52:42 +0900

    [Finish]chapter4 まとめて扱う ー配列

---
+++ chapter4α/chapter4 practice.rb
@@ -0,0 +1,55 @@
+# 4-1
+# 問1
+p ["コーヒー", "カフェラテ"]
+
+# 4-2
+# 問2
+drinks = ["コーヒー", "カフェラテ", "モカ"]
+
+# 問3
+drinks = ["コーヒー", "カフェラテ", "モカ"]
+puts drinks[1]
+
+# 問4
+drinks = ["コーヒー", "カフェラテ", "モカ"]
+puts drinks.first
+puts drinks.last
+
+# 4-3
+# 問5
+drinks = ["コーヒー", "カフェラテ"]
+p drinks.push("モカ")
+
+# 問6
+numbers = [2, 3]
+p numbers.unshift(1)
+
+# 問7
+p [1, 2] + [3, 4]
+
+# 4-4
+# 問8
+drinks = ["ティーラテ", "カフェラテ", "抹茶ラテ"]
+drinks.each do |drink|
+  puts drink
+end
+
+# 問9
+drinks = ["ティーラテ", "カフェラテ", "抹茶ラテ"]
+drinks.each do |drink|
+  puts "#{drink}お願いします"
+end
+
+# 問10
+sum = 0
+numbers = [1, 2, 3]
+numbers.each do |x|
+  sum += x
+end
+puts sum
+
+# 問11
+drinks = []
+drinks.each do |drink|
+  puts drink
+end
